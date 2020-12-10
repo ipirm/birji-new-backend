b650dee74196ce286daec1c4810276a6f7e70a5e
Ilham <ilham.pirm@gmail.com>
2020-12-10 20:36:43 +0400

    Twitter Auth

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 import {ValidationPipe} from "@nestjs/common";
-
+import * as session from 'express-session';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -14,6 +14,13 @@ async function bootstrap() {
         .build();
     app.useGlobalPipes(new ValidationPipe());
     const document = SwaggerModule.createDocument(app, options);
+    app.use(
+        session({
+            secret: 'birjiRfSecret',
+            resave: false,
+            saveUninitialized: false,
+        }),
+    );
     SwaggerModule.setup('api', app, document);
     await app.listen(process.env.PORT || 3000);
 }
