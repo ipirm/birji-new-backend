b9daa3e6ea38f17cba1ff31399bdebf78abe454a
Ilham <ilham.pirm@gmail.com>
2020-12-10 19:59:26 +0400

    Twitter Auth

--- src/auth/social/twitter.strategy.ts
+++ src/auth/social/twitter.strategy.ts
@@ -1,7 +1,7 @@
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, VerifyCallback } from 'passport-twitter';
+import {PassportStrategy} from '@nestjs/passport';
+import {Strategy} from 'passport-twitter';
 
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
 import {UserService} from "../../user/user.service";
 
 @Injectable()
@@ -12,13 +12,11 @@ export class TwitterStrategy extends PassportStrategy(Strategy, 'twitter') {
             consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
             callbackURL: process.env.TWITTER_CALLBACK_URL,
             scope: ['email', 'profile'],
-        });
-    }
-    async validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
-        console.log(profile)
-        const user = await this.userService.findOrCreate(
-            profile,
-        );
-        done(null, user);
+        }, async (accessToken: any,
+                  refreshToken: any,
+                  params: any,
+                  profile: any) => {
+             await this.userService.findOrCreate(profile)
+        })
     }
 }
