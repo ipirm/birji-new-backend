b3fe7f7ea6de1ca136fb4cc02cf10c4c33e6c5ff
Ilham <ilham.pirm@gmail.com>
2020-12-14 03:45:17 +0400

    Fixed app

--- src/database/orm.config.ts
+++ src/database/orm.config.ts
@@ -27,14 +27,12 @@ const config: any = {
     username: 'cdiimlezylnkrx',
     password: '44c74b5ef1d1c49a866f3a40cabdceb802042de251cb9375c0fe8c9bec77c5ba',
     database: 'd7dfem11uggn5m',
-    entities: ["dist/database/**/*.entity.js"],
-    factories: [ "dist/database/factories/**/*.js" ],
-    seeds: [ "dist/database/seeds/**/*.js"],
-    migrations: ["dist/migration/*.js"],
+    entities: ['dist/**/*.entity.js'],
+    migrations: ["dist/migrations/*{.ts,.js}"],
     migrationsTableName: "migrations_typeorm",
     migrationsRun: true,
     synchronize: true,
-    logging: ['error'],
+    logging: true,
     cli: {
         migrationsDir: "migrations"
     }
