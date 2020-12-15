d4b5d4c752d24bd00e83843dbc3b78963b35940d
Ilham <ilham.pirm@gmail.com>
2020-12-16 01:55:46 +0400

    Twitter Test

--- src/auth/social/twitter.strategy.ts
+++ src/auth/social/twitter.strategy.ts
@@ -12,9 +12,10 @@ export class TwitterStrategy extends PassportStrategy(Strategy, 'twitter') {
             consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
             callbackURL: process.env.TWITTER_CALLBACK_URL,
             scope: ["email"],
-            profileFields: ['id', 'displayName', 'name','emails','email']
+            profileFields: ['id', 'displayName', 'name', 'emails', 'email']
         }, async (token, tokenSecret, profile) => {
-             await this.userService.findOrCreate(profile)
+            console.log(profile);
+            await this.userService.findOrCreate(profile)
         })
     }
 }
